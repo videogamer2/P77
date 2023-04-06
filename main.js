13f48cd6de7308d677816932b373f152fdfe4546
videogamer2 <116688075+videogamer2@users.noreply.github.com>
2023-04-06 15:48:46 -0300

    uhh yeah
    yup mhm

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("oi pai");
+console.log("oi mãe");
+var firstname="Geraldo ";
+var lastname="Oliveira";
+var fullname= firstname.concat(lastname);
+console.log(fullname);
+var num1=(5);
+var num2=(15);
+var num3=(num1 + num2);
+console.log("A soma dos dois números é: "+num3);
+var num4=(num1 - num2);
+console.log("A subtração dos dois números é: "+num4);
+var num5=(num1 * num2);
+console.log("A multipicação dos dois números é: "+num5);
+var num6=(num1 / num2);
+console.log("A divisão dos dois números é: "+num6);
+//não está funcionando?
+//não tenho muita certeza sobre o porquê...
